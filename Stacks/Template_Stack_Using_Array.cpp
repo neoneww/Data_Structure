fde0e2057485c585c0a0a066b37348f782a46b32
Sourav Sharma <51079951+neoneww@users.noreply.github.com>
2020-09-18 21:00:11 +0530

    Create Template_Stack_Using_Array.cpp

---
+++ Stacks/Template_Stack_Using_Array.cpp
@@ -0,0 +1,60 @@
+#include<climits>
+template <typename T>
+class stackUsingArray{
+    T *data;
+    int index;
+    int capacity;
+public:
+    stackUsingArray()
+    {
+        data = new T[4];
+        index = 0;
+        capacity = 4;
+    }
+    int size()
+    {
+        return index;
+    }
+    bool isempty()
+    {
+        return index == 0;
+    }
+    void push(T elem)
+    {
+        if(index == capacity)
+        {
+            T *newData;
+            newData = new T[2*capacity];
+            for(int i = 0;i < capacity;i++)
+            {
+                newData[i] = data[i];
+            }
+            capacity = capacity*2;
+            //cout<<"Stack Is Full "<<"\n";
+            //return;
+            delete []data;
+            data = newData;
+        }
+        data[index] = elem;
+        index++;
+    }
+    int pop()
+    {
+        if(isempty())
+        {
+            cout<<"Stack Is Empty "<<"\n";
+            return INT_MIN;
+        }
+        index--;
+        return data[index];
+    }
+    T top()
+    {
+         if(isempty())
+        {
+            cout<<"Stack Is Empty "<<"\n";
+            return INT_MIN;
+        }
+        return data[index-1];
+    }
+};
