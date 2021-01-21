b03623bd185186d6820af9fd78bf32c550d66141
Sourav Sharma <51079951+neoneww@users.noreply.github.com>
2021-01-21 13:02:08 +0530

    Update InvertTree.cpp

--- Tree/Method/InvertTree.cpp
+++ Tree/Method/InvertTree.cpp
@@ -1,34 +1,31 @@
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-         if(root == NULL)
-         {
-             return root;
-         }
-         else if(root->left == NULL && root->right == NULL)
-         {
-             return root;
-         }
-         else if(root->left != NULL && root->right ==NULL)
-         {
-             root->right = root->left;
-             root->left = NULL;
-             invertTree(root->right);
-         }
-         else if(root->right != NULL && root->left ==NULL)
-         {
-             root->left = root->right;
-             root->right = NULL;
-             invertTree(root->left);
-         }
-         else{
-            TreeNode *a = root->left;
-             root->left = root->right;
-             root->right = a;
-             invertTree(root->left);
-             invertTree(root->right);
-         }
+/**
+ * class Tree {
+ *     public:
+ *         int val;
+ *         Tree *left;
+ *         Tree *right;
+ * };
+ */
+//inverting a binary tree
+
+Tree* invertTree(Tree* root)
+{
+    if(root == NULL)
+    {
         return root;
-        
     }
-};
+    Tree* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
+
+Tree* solve(Tree* root) {
+    if(root == NULL)
+    {
+        return root;
+    }
+    return invertTree(root);
+}
