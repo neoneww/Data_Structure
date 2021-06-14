d1edd507fa96fc336a49e22d3c24cb6fe2d8f71d
Sourav Sharma <51079951+neoneww@users.noreply.github.com>
2021-06-14 12:39:23 +0530

    Create 78. Subsets.cpp

---
+++ recursion and backtracking/78. Subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+        
+    void makeChoice(int i,set<vector<int>> &ans,vector<int> &nums,vector<int> &temp)
+    {
+        if(i == nums.size())
+        {
+            ans.insert(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        makeChoice(i+1,ans,nums,temp);
+        temp.pop_back();
+        makeChoice(i+1,ans,nums,temp);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> finAns;
+        set<vector<int>> ans;
+        vector<int> temp;
+        makeChoice(0,ans,nums,temp);
+        for(auto i:ans)
+        {
+            finAns.push_back(i);
+        }
+        
+        return finAns;
+    }
+};
