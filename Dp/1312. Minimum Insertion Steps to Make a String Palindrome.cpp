59dea93e26d4645c601b0639d161173b7fa2b444
Sourav Sharma <51079951+neoneww@users.noreply.github.com>
2021-06-23 12:49:45 +0530

    Create 1312. Minimum Insertion Steps to Make a String Palindrome.cpp

---
+++ Dp/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+    int lps(string &a, string &b, int n, int m, vector<vector<int>> &dp) {
+        if (n == 0 || m == 0) return 0;
+        if (dp[n][m] != -1) return dp[n][m];
+        if (a[n - 1] == b[m - 1]) {
+            return dp[n][m] = 1 + lps(a, b, n - 1, m - 1, dp);
+        }
+        return dp[n][m] = max(lps(a, b, n - 1, m, dp), lps(a, b, n, m - 1, dp));
+    }
+    int minInsertions(string a) {
+        if (a.length() == 0) return 0;
+        string b = a;
+        reverse(b.begin(), b.end());
+        int n = a.length(), m = b.length();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        lps(a, b, n, m, dp);
+        return a.length() - dp[n][m];
+    }
+};
