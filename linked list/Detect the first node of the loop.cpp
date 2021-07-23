a83a274358fbef7e194ac23b96fb465c1397dc5d
Sourav Sharma <51079951+neoneww@users.noreply.github.com>
2021-07-23 17:45:25 +0530

    Create Detect the first node of the loop.cpp

---
+++ linked list/Detect the first node of the loop.cpp
@@ -0,0 +1,24 @@
+Node *firstNode(Node *head)
+{
+    if(head == NULL || head->next == NULL) return NULL;
+
+    Node* slow = head,*fast = head;
+
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            break;
+        }
+    }
+
+    if(fast == NULL || fast->next == NULL) return NULL;
+
+    slow = head;
+    while(slow != fast){
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return slow;
+}
